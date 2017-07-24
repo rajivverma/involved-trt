28f2aec80b8a1e86f104d38e043905865c6dde7e
jignasha <jignashatrt@gmail.com>
2017-07-24 15:37:41 +0530

    grapg page work

--- src/app/dashboard/performance/performance.component.ts
+++ src/app/dashboard/performance/performance.component.ts
@@ -19,6 +19,10 @@ export class PerformanceComponent implements OnInit {
   public subjectDetails: any = {};
   public intervalCount = 0;
   public intervalsId: any = [];
+  public studentDetailInterval: any;
+  public monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+  ];
   constructor(private router: Router,
     private performanceService: PerformanceService,
     private dashboardService: DashboardService,
@@ -74,24 +78,69 @@ export class PerformanceComponent implements OnInit {
         // alert('something wrong');
       });
   }
+  getColor(val) {
+    if (val == 0) {
+      return '#3c3c3c';
+    } else if (val == 1) {
+      return '#6bc04b';
+    } else if (val == 2) {
+      return '#ff7800';
+    } else {
+      return '#ff3c1f';
+    }
+  }
   getSubjectDetails(subject) {
     const id = localStorage.getItem('userid');
     this.performanceService.getStudentPerformanceData(id, subject.Id).subscribe(
       (data) => {
         this.subjectDetails = data;
         this.subjectDetails.subject = subject;
-        console.log(this.subjectDetails);
         const d = document.getElementById('performance-modal');
         d.className += ' in';
         const GradeSet = [];
         const GradeResults = [];
+        let date;
         for (let i = 0; i < this.subjectDetails.GradeSet.length; i++) {
           GradeSet.push(this.subjectDetails.GradeSet[i].Code);
         }
-        for (let i = 0; i < this.subjectDetails.GradeResults.length; i++) {
-          GradeResults.push(this.subjectDetails.GradeResults[i].Code);
+        for (let i = 0; i < this.subjectDetails.XaxisGrades.length; i++) {
+          date = new Date(this.subjectDetails.XaxisGrades[i].Date);
+          date = this.monthNames[date.getMonth()] +' '+ date.getFullYear();
+          GradeResults.push(date);
         }
-        this.options = this.piChart.getPieChartData(GradeSet);
+        console.log(data);
+        this.options = this.piChart.getPieChartData(GradeSet, GradeResults);
+        let that = this;
+        setTimeout(function () {
+          document.getElementById('sd' + that.subjectDetails.subject.Id).children[1].children[1].className += ' subject-name-active';
+          document.getElementById('sd' + that.subjectDetails.subject.Id).children[0].children[0].className += ' teacher-image-active';
+          (function () {
+            let d = document.getElementById('sd' + that.subjectDetails.subject.Id).children[1];
+            let dp = document.getElementById('sd' + that.subjectDetails.subject.Id).children[0];
+            let iCount = 0;
+            if (d.childElementCount > 2) {
+              iCount++;
+              that.studentDetailInterval = setInterval(function () {
+                if (iCount === 1) {
+                  d.children[iCount + 1].className = 'second';
+                  d.children[iCount].className = 'second subject-name-active';
+                  dp.children[iCount - 1].className = 'teacher-image teacher-image-active';
+                  dp.children[iCount].className = 'teacher-image';
+                } else {
+                  d.children[iCount - 1].className = 'second';
+                  d.children[iCount].className = 'second subject-name-active';
+                  dp.children[iCount - 2].className = 'teacher-image';
+                  dp.children[iCount - 1].className = 'teacher-image teacher-image-active';
+                }
+                if (iCount === (d.childElementCount - 1)) {
+                  iCount = 1;
+                } else {
+                  iCount++;
+                }
+              }, 5000);
+            }
+          })();
+        });
       },
       (err) => {
         // alert('something wrong');
@@ -100,6 +149,7 @@ export class PerformanceComponent implements OnInit {
   closeModal() {
     const d = document.getElementById('performance-modal');
     d.className = d.className.replace('in', '');
+    clearInterval(this.studentDetailInterval);
   }
 
 }
