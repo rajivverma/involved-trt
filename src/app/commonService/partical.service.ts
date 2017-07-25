14c27745d87f1c783aa7e584c6472c598b99cf6d
jignasha <jignashatrt@gmail.com>
2017-07-25 16:55:04 +0530

    repulse property added

--- src/app/commonService/partical.service.ts
+++ src/app/commonService/partical.service.ts
@@ -20,9 +20,9 @@ export class ParticalService {
       particles: {
         number: {
           value: 100,
-            density: {
+          density: {
             enable: true,
-              value_area: 800
+            value_area: 800
           }
         },
         color: {
@@ -30,91 +30,91 @@ export class ParticalService {
         },
         shape: {
           type: 'circle',
-            stroke: {
+          stroke: {
             width: 0,
-              color: '#54c9e8'
+            color: '#54c9e8'
           },
           polygon: {
             nb_sides: 5
           },
           image: {
             src: 'img/github.svg',
-              width: 100,
-                height: 100
+            width: 100,
+            height: 100
           }
         },
         opacity: {
           value: 0.5,
-            random: false,
-              anim: {
+          random: false,
+          anim: {
             enable: false,
-              speed: 1,
-                opacity_min: 0.1,
-                  sync: false
+            speed: 1,
+            opacity_min: 0.1,
+            sync: false
           }
         },
         size: {
           value: 3,
-            random: true,
-              anim: {
+          random: true,
+          anim: {
             enable: false,
-              speed: 0.6,
-                size_min: 0.1,
-                  sync: false
+            speed: 0.6,
+            size_min: 0.1,
+            sync: false
           }
         },
         line_linked: {
           enable: true,
-            distance: 100,
-              color: '#54c9e8',
-                opacity: 0.4,
-                  width: 0
+          distance: 100,
+          color: '#54c9e8',
+          opacity: 0.4,
+          width: 0
         },
         move: {
           enable: true,
-            speed: 6,
-              direction: 'none',
-                random: false,
-                  straight: false,
-                    out_mode: 'out',
-                      bounce: false,
-                        attract: {
+          speed: 6,
+          direction: 'none',
+          random: false,
+          straight: false,
+          out_mode: 'out',
+          bounce: true,
+          attract: {
             enable: false,
-              rotateX: 600,
-                rotateY: 1200
+            rotateX: 600,
+            rotateY: 1200
           }
         }
       },
       interactivity: {
         detect_on: 'canvas',
-          events: {
+        events: {
           onhover: {
             enable: true,
-              mode: 'grab'
+            mode: 'repulse'
           },
           onclick: {
             enable: true,
-              mode: 'push'
+            mode: 'push'
           },
           resize: true
         },
         modes: {
           grab: {
-            distance: 240,
-              line_linked: {
+            distance: 400,
+            line_linked: {
               opacity: 1
             }
           },
           bubble: {
-            distance: 100,
-              size: 40,
-                duration: 2,
-                  opacity: 8,
-                    speed: 10
+            distance: 400,
+            size: 80,
+            duration: 2,
+            opacity: 0.8,
+            speed: 3
           },
           repulse: {
-            distance: 100,
-              duration: 0.4
+            distance: 150,
+            duration: 0.4
           },
           push: {
             particles_nb: 4
